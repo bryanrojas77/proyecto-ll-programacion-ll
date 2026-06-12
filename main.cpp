abd81400915df479c372fc228c6a9d5ebac2e28f
Bryan <bryan.rojas.barrantes@est.una.ac.cr>
2026-06-12 01:39:10 -0600

    inicio de main

--- main.cpp
+++ main.cpp
@@ -1,20 +1,44 @@
 #include <iostream>
+#include "WorldLoader.h"
+#include "Simulation.h"
+#include "EventLog.h"
+#include "ReportGenerator.h"
+#include <stdexcept>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main() {
+    try {
+        const std::string dataDir   = "data";
+        const std::string outputDir = "output";
 
-int main()
-{
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
+        auto world = WorldLoader::loadWorld(dataDir);
+        auto hero  = WorldLoader::loadHero(dataDir);
 
-    const auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+        World* worldPtr = world.get();
 
-    for (int i = 1; i <= 5; i++)
-    {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+        EventLog eventLog{outputDir + "/log.txt"};
+
+        Simulation simulation{std::move(hero), std::move(world)};
+        simulation.attachObserver(&eventLog);
+        simulation.run();
+
+        eventLog.flush();
+
+        ReportGenerator report{outputDir + "/report.txt"};
+        report.generate(simulation, *worldPtr);
+
+        std::cout << "\nLog saved to:    " << outputDir << "/log.txt\n";
+        std::cout << "Report saved to: " << outputDir << "/report.txt\n";
+
+    } catch (const FileFormatError& e) {
+        std::cerr << "[FILE ERROR] " << e.what() << "\n";
+        return 2;
+    } catch (const std::invalid_argument& e) {
+        std::cerr << "[INVALID ARGUMENT] " << e.what() << "\n";
+        return 3;
+    } catch (const std::exception& e) {
+        std::cerr << "[ERROR] " << e.what() << "\n";
+        return 1;
     }
 
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
